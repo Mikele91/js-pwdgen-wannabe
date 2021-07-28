b55e9a89e089b426e3897a41251980322140f72a
Mikele91 <michele.amoruso91.8@gmail.com>
2021-07-28 13:33:29 +0200

    esercizio terminato

---
+++ script/script.js
@@ -0,0 +1,10 @@
+var nome = prompt("inserisci il tuo nome");
+
+var cognome = prompt("inserisci il tuo cognome");
+
+var colore = prompt("inserisci il tuo colore preferito");
+
+const numClass = parseInt("40");
+
+document.getElementById("password-generator").innerHTML = nome + cognome + colore +numClass;
+
